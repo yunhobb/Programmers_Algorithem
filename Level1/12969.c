0638d9390b577ad23784066242dd46807f10eea5
YunHo Jung <a01049048063@gmail.com>
2022-07-24 02:07:17 +0900

    직사각형 별찍기

---
+++ Level1/12969.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//직사각형 별찍기
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    for(int i=0;i<b;i++){
+        for(int j=0;j<a;j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
